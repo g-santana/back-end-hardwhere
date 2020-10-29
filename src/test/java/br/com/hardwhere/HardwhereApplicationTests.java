b0c01759eb3b5287ee9e014474e5a0b3cd9cb72c
Gabriel Santana Barroso <gabriel.barroso@dtidigital.com.br>
2020-10-29 20:14:23 -0300

    Criação da estrutura inicial do back-end.

---
+++ src/test/java/br/com/hardwhere/HardwhereApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hardwhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwhereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
